e3842635c30fdc0a9859c53efc2cc30147974d96
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2017-12-21 08:23:36 -1000

    debugged, still working

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -10,74 +10,127 @@ var global = window || GLOBAL;
 global.bruhdash = {
 
   // returns the first element of an array
-  first: function () {
-      
+  first: function (arr) {
+    return arr[0];
   },
 
   // returns the last element of an array
-  last: function () {
-
+  last: function (arr) {
+    return arr[arr.length-1];
   },
 
   // returns the index of the first matching element from left to right
-  indexOf: function () {
-
+  indexOf: function (arr, elem) {
+    return arr.indexOf(elem);
   },
 
   // returns the index of the first matching element from right to left
-  lastIndexOf: function () {
-
+  lastIndexOf: function (arr, elem) {
+    return arr.lastIndexOf(elem);
   },
 
   // returns an array with all elements except for the last element
-  initial: function () {
-
+  initial: function (arr) {
+    arr.pop();
+    return arr;
   },
   
   // returns an array with all falsey values removed
-  compact: function() {
-
+  compact: function(arr) {
+    // var falseyDef = [false, null, undefined, 0, NaN, '', ""];
+    // for(var i = 0; i<arr.length; i++){
+    //   for(var f = 0; f<falseyDef.length; f++){
+    //     if(arr[i] === falseyDef[f]){
+    //       arr.splice(i,1);
+    //     }else if(Boolean(arr[i]) === false){
+    //       arr.splice(i,1);
+    //     }
+    //   }
+    // }
+    return arr.filter(Boolean);
   },
 
   // creates a slice of an array from the start index up to but not including the end index
-  slice: function () {
-
+  slice: function (arr, index) {
+    arr.pop();
+    return arr.splice(index);
   },
 
   // returns a slice of array with n elements dropped from the beignning
-  drop: function(){
-
+  drop: function(arr, n){
+    if(n === undefined){
+      return arr.splice(1);
+    }else{
+      return arr.splice(n);
+    }
   },
 
   // returns a slice of array with n elements dropped from the end
-  dropRight: function() {
-
+  dropRight: function(arr, n) {
+    if(n === undefined){
+      arr.pop();
+      return arr;
+    }else{
+      return arr.slice(0,arr.length-n);
+    }
   },
 
   // creates a slice of an array with n elements taken from the beginning
-  take: function () {
-
+  take: function (arr, n) {
+    if(n === undefined){
+      return arr.slice(0,1);
+    }else{
+      return arr.slice(0,n);
+    }
   },
 
   // creates a slice of an array with n elements taken from the end
-  takeRight: function () {
-
+  takeRight: function (arr, n) {
+    if(n === undefined){
+      return arr.splice(arr.length-1);
+    }else{
+      return arr.splice(arr.length-n);
+    }
   },
 
   // fills elements of array with specified value from the start index
   // up to but not including the end index
-  fill: function() {
-
+  fill: function(arr, val, ind1, ind2) {
+    if(ind1 === undefined){
+      ind1 = 0;
+      ind2 = arr.length;
+    }
+    for(var i = ind1; i<ind2; i++){
+      arr.splice(i,1,val);
+    }
+    return arr;
   },
 
   // removes all given values from an array
-  pull: function () {
-
+  pull: function (arr, ...vals) {
+    console.log(vals);
+    if(vals !== undefined){
+      for(var j = 0; j<vals.length; j++){
+        for(var i = arr.length; i=>0;i--){
+          var ind = arr.indexOf(vals[j]);
+          arr.splice(ind,1);
+        }
+      }
+      return arr;
+    }
+    else{
+      return arr;
+    // }
   },
 
   // removes elements of an array corresponding to the given indices
-  pullAt: function () {
-
+  pullAt: function (arr, inds) {
+    var result = [];
+    for(var i = 0; i<inds.length; i++){
+      result.push(arr[inds[i]]);
+      arr.splice(inds[i],1);
+    }
+    return result;
   },
 
   // creates an array excluding all the specified values
